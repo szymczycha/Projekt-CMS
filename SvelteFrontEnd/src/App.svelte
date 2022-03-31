f2623fcc1704f5929abb57239913383fbc46f3c9
Szymon Konieczny <szymczycha266@gmail.com>
2022-03-31 19:26:52 +0200

    Co-authored-by: AntLeszcz <AntLeszcz@users.noreply.github.com>

---
+++ SvelteFrontEnd/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import loginPage from "./routes/loginPage.svelte";
+	import Tailwindcss from "./Tailwindcss.svelte";
+	import Router from "svelte-spa-router";
+  </script>
+  
+  <Tailwindcss />
+  
+  <Router
+	routes={{
+	  "/": loginPage
+	}}
+  />
+  <main />
+  
---
+++ SvelteFrontEnd/src/routes/loginPage.svelte
@@ -0,0 +1,4 @@
+<label for="login">Login</label>
+<input name="login" type="text"/>
+<label for="password">Password</label>
+<input name="password" type="password"/>
