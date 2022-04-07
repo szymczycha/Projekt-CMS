8c58f84be85f388dbcb04e41851688d0761b73cd
Szymon Konieczny <szymczycha266@gmail.com>
2022-04-07 16:04:35 +0200

    ?

--- SvelteFrontEnd/src/routes/loginPage.svelte
+++ SvelteFrontEnd/src/routes/loginPage.svelte
@@ -1,19 +1,16 @@
 <script>
-    async function logIn() {
-        let body = JSON.stringify({
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value,
-        });
-        console.log(body);
+    async function logIn(){
         await fetch("http://127.0.0.1:5000/logIn", {
             method: "POST",
+            mode: 'no-cors',
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'text/plain'//'application/json'
             },
-            body: body,
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            body: JSON.stringify({
+                username: document.getElementById("username").value,
+                password: document.getElementById("password").value
+            })
+        }).then(response => response.json()).then(data => console.log(data))
     }
 </script>
 
@@ -21,11 +18,11 @@
     <h1>CMS Project</h1>
     <div class="loginForm">
         <label for="username">Username</label>
-        <input id="username" name="username" type="text" />
+        <input id="username" name="username" type="text"/>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <input id="password" name="password" type="password"/>
         <div style="display: flex; justify-content: space-evenly; width:100%;">
-            <button on:click={logIn}>Log In</button>
+            <button on:click={logIn}>Log In</button>  
             <a href="/#/registerPage" class="aAsButton">Register</a>
         </div>
     </div>
