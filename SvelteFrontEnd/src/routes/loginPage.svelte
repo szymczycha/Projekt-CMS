a2c9a120670d5ec6d3113b2651a976453772e47a
AntLeszcz <antek.leszczynski@gmail.com>
2022-04-07 16:03:32 +0200

    added class to button

--- SvelteFrontEnd/src/routes/loginPage.svelte
+++ SvelteFrontEnd/src/routes/loginPage.svelte
@@ -1,17 +1,5 @@
 <script>
     async function logIn() {
-<<<<<<< HEAD
-        await fetch("http://127.0.0.1:5000/logIn", {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-                "Content-Type": "text/plain", //'application/json'
-            },
-            body: JSON.stringify({
-                username: document.getElementById("username").value,
-                password: document.getElementById("password").value,
-            }),
-=======
         let body = JSON.stringify({
             username: document.getElementById("username").value,
             password: document.getElementById("password").value,
@@ -23,7 +11,6 @@
                 "Content-Type": "application/json",
             },
             body: body,
->>>>>>> e452bcbeebce7f0dc0d99a993bd0921e20266154
         })
             .then((response) => response.json())
             .then((data) => console.log(data));
@@ -38,11 +25,7 @@
         <label for="password">Password</label>
         <input id="password" name="password" type="password" />
         <div style="display: flex; justify-content: space-evenly; width:100%;">
-<<<<<<< HEAD
             <button on:click={logIn} class="aAsButton">Log In</button>
-=======
-            <button on:click={logIn}>Log In</button>
->>>>>>> e452bcbeebce7f0dc0d99a993bd0921e20266154
             <a href="/#/registerPage" class="aAsButton">Register</a>
         </div>
     </div>
